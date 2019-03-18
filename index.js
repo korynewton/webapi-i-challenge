b99fc404cd406ac66e72468add517f6fb8da8061
korynewton <korynewtondev@gmail.com>
2019-03-18 12:41:11 -0700

    updated error message on GET request

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ server.get('/api/users', (req, res) => {
         res.status(200).json(users);
     })
     .catch(err => {
-        res.status(500).json({ message: 'error retrieving users' });
+        res.status(500).json({ error: "The users information could not be retrieved." });
     })
 })
 
