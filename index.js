af8b1435dc12e095228aea14ba871126057a0d37
korynewton <korynewtondev@gmail.com>
2019-03-18 12:54:14 -0700

    GET user by ID working

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const server = express();
 
 server.use(express.json()); //for PUT and POST requests
 
+
 server.get('/', (req, res) => {
     res.send('Working?')
 });
 
+//GET Users:
 server.get('/api/users', (req, res) => {
     db.find()
     .then(users => {
@@ -17,6 +19,22 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => {
         res.status(500).json({ error: "The users information could not be retrieved." });
+    })
+})
+
+//GET User by ID:
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id
+    db.findById(id)
+    .then(user => {
+        if (user === undefined) {
+            res.status(404).json({ message: "The user with the specified ID does not exist." })
+        }
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({ error: "The user information could not be retrieved." })
     })
 })
 
