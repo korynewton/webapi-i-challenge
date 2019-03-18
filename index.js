9a83994c3787a27fdfd8369e20d894f38affda7d
korynewton <korynewtondev@gmail.com>
2019-03-18 12:38:21 -0700

    GET users request working

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ server.get('/', (req, res) => {
     res.send('Working?')
 });
 
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        res.status(200).json(users);
+    })
+    .catch(err => {
+        res.status(500).json({ message: 'error retrieving users' });
+    })
+})
+
 server.listen(4000, () => {
     console.log('\n** Project API up and running on port 4k **');
   });
