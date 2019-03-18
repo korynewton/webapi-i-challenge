ddb63aa10cc3f19433a7b3fcab20700158cc94d1
korynewton <korynewtondev@gmail.com>
2019-03-18 12:23:44 -0700

    server initialized, express added, server functioning

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json()); //for PUT and POST requests
+
+server.get('/', (req, res) => {
+    res.send('Working?')
+});
+
+server.listen(4000, () => {
+    console.log('\n** Project API up and running on port 4k **');
+  });
